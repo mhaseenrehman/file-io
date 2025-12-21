d5dbbffa8757827299cca4216e50b70b09134e53
Yrios <mhaseenrehman@gmail.com>
2025-12-21 16:33:28 +0000

    (Frontend): Prototype Completed

---
+++ resources/js/Components/CompressionInputs.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="controls p-8">
+        <div class="control-group dark:text-gray-400 p-2">
+            <label for="compressionRange">
+                Compression Level: <span id=compressionLevel>{{ chosenImage.quality }}</span>%
+            </label>
+            <input class="col-start-1 row-start-1 w-full accent-red-500 bg-transparent rounded-lg" type="range" name="compressionRange" id="compressionRange" min="10" max="90" step="5" v-model="chosenImage.quality">
+        </div>
+
+        <div class="control-group dark:text-gray-400 p-2">
+            <label for="compressionFormat">
+                Format
+            </label>
+            <select class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-gray-800 py-1.5 pr-7 pl-3 text-base text-gray-400 *:bg-gray-800 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" name="compressionFormat" id="compressionFormat" v-model="chosenImage.format">
+                <option value="webp">webp</option>
+                <option value="jpeg">jpeg</option>
+                <option value="png">png</option>
+            </select>
+        </div>
+
+        <div class="control-group dark:text-gray-400 p-2">
+            <label for="compressionWidth">
+                Width
+            </label>
+            <input class="block min-w-0 w-full rounded-md grow bg-gray-800 py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6" type="number" name="compressionWidth" id="compressionWidth" v-model="chosenImage.width" placeholder="N/A" >
+        </div>
+    </section>
+</template>
+
+
+
+<script setup>
+    const props = defineProps({
+        chosenImage: {
+            type: Object,
+            required: true
+        }
+    });
+</script>
+
+<!-- <style scoped></style> -->
---
+++ resources/js/Pages/Functionality/CompressionHome.vue
@@ -0,0 +1,42 @@
+<template lang="">
+    <div class="flex flex-row min-h-screen justify-center items-center dark:bg-gray-800">
+        <form>
+            <CompressionInputs :chosenImage="data.chosenImage"/>
+            <div class="p-2" @drop.prevent="handleDrop">
+                <label for="imagesInput" class="drop-zone-label dark:text-gray-400 border-2 border-dashed p-8" >
+                    Drag and Drop images here or Click to select from Directory
+                </label>
+                <!-- <input type="file" multiple name="imagesInput" id="imagesInput" accept="image/*" hidden> -->
+                <input type="file" name="imagesInput" id="imagesInput" accept="image/*" hidden @change="handleFileChange">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+    import { reactive } from 'vue';
+    import CompressionInputs from '@/Components/CompressionInputs.vue';
+
+    const initial_image_state = {
+        fileSelected: null,
+        imageUrl: null,
+        format: "webp",
+        quality: 50,
+        width: null
+    }
+
+    const data = reactive({
+        chosenImage: {...initial_image_state},
+        fileInfo: null
+    });
+
+    const handleDrop = (event) => {
+        console.log(event.dataTransfer.files[0]);
+    }
+
+    const handleFileChange = (event) => {
+        console.log(event.target.files[0]);
+    }
+</script>
+
+<!-- <style scoped></style> -->
