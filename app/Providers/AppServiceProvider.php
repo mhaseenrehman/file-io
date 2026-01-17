5e75b3d4c9b0a21ce1de7504ba6a9d24a5dace23
Yrios <mhaseenrehman@gmail.com>
2026-01-17 18:41:47 +0000

    (Added): Dockerfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Models\ImageFile;
 use App\Observers\ImageFileObserver;
 
@@ -22,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
         Vite::prefetch(concurrency: 3);
         // Implement RateLimiter
 
