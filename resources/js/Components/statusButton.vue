ddc951e2d8ce7ec4979f91d706d6c4be3648df68
Yrios <mhaseenrehman@gmail.com>
2026-01-26 16:39:35 +0000

    (Frontend): Green download button

--- resources/js/Components/statusButton.vue
+++ resources/js/Components/statusButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="flex mx-8 my-4 size-auto text-white font-bold text-blue-100 no-underline rounded-full text-center items-center justify-center" :class="downloadableLink === 'Pending' || 'Waiting' ? 'bg-stone-500' : 'bg-green-500'" :href="downloadableLink" :download="filename">
+    <a class="flex mx-8 my-4 size-auto text-white font-bold text-blue-100 no-underline rounded-full text-center items-center justify-center" :class="downloadableLink === 'Waiting' ? 'bg-stone-500' : downloadableLink === 'Pending' ? 'bg-stone-500' : 'bg-green-500'" :href="downloadableLink" :download="filename">
         <!-- <button class="text-white font-bold py-2 px-4 rounded-full" :class="downloadableLink === 'Pending' || 'Waiting' ? 'bg-stone-500' : 'bg-green-500'">{{ downloadableLink === 'Waiting' ? 'Waiting' : downloadableLink === 'Pending' ? 'Pending' : 'Download' }}
 
         </button> -->
