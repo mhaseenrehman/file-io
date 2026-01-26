7f3a97eb9018c0e8554a27585a02a91ea75251b8
Yrios <mhaseenrehman@gmail.com>
2026-01-26 17:18:15 +0000

    (Cron): Cron Job

--- scheduler.sh
+++ scheduler.sh
@@ -1,1 +1,10 @@
-* * * * * php artisan schedule:run >> /dev/null 2>&1
+#!/bin/bash
+# Make sure this file has executable permissions, run `chmod +x railway/run-cron.sh`
+
+# This block of code runs the Laravel scheduler every minute
+while [ true ]
+    do
+        echo "Running the scheduler..."
+        php artisan schedule:run --verbose --no-interaction &
+        sleep 60
+    done
